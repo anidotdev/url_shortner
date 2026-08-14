b369c23582095a63ea087917e0adba869f274761
animeshhq <animeshhq@gmail.com>
2026-08-14 13:59:56 +0530

    added mongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+require('dotenv').config()
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+console.log(process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("MongoDB connection error: ", err))
 
 app.get('/', async (req, res) => {
   const urls = [];
